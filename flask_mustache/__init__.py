a89698a6b27db50ad3d877d810179b7868aff681
Bradley Wright <brad@intranation.com>
2012-02-29 23:04:26 +0000

    Fix template loader to make it work properly

--- flask_mustache/__init__.py
+++ flask_mustache/__init__.py
@@ -1,16 +1,19 @@
 # flask-mustache Flask plugin
+import os
+
 from flask import current_app
 
 # context processor
 def mustache_templates():
     "Returns the content of all Mustache templates in the Jinja environment"
-    all_templates = current_app.jinja_loader.list_templates('.mustache')
+    all_templates = current_app.jinja_loader.list_templates()
     # returns a dictionary of {template name:content}
     mustache_templates = {}
-    for template in all_templates:
-        # TODO: need to remove Jinja load path from path and leave
-        # trailing path
-        pass
+    for template_name in all_templates:
+        # TODO: make this configurable
+        if template_name.endswith('mustache'):
+            template, _, _ = current_app.jinja_loader.get_source(current_app.jinja_env, template_name)
+            mustache_templates[template_name] = template
     return {'mustache_templates': mustache_templates}
 
 # template helper function
