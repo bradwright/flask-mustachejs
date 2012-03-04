23e953dc28f71e0cd4d841dc895e08129ea06c34
Bradley Wright <brad@intranation.com>
2012-03-04 21:15:00 +0000

    Bump to 0.4.0 since we support mustacheAsFunction now

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Flask-MustacheJS',
-    version='0.3.1',
+    version='0.4.0',
     url='https://github.com/bradleywright/flask-mustache',
     license='BSD',
     author='Bradley Wright',
